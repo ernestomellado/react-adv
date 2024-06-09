e8d9663b7703ab1e75c66aa98a8febc029f0a7bd
ernesto <ernestomelladovallecillo@gmail.com>
2024-06-09 14:40:14 -0700

    adding component compount

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../logo.svg'
+import ShoppingPage from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
     return (
@@ -11,7 +12,7 @@ export const Navigation = () => {
                     <img src={ logo } alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink to="/ShoppingPage" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Shopping Page</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -26,9 +27,9 @@ export const Navigation = () => {
                 <Routes>
                     <Route path="about" element={ <h1>About Page</h1> } />
                     <Route path="users" element={ <h1>Users Page</h1> } />
-                    <Route path="home" element={ <h1>Home Page</h1> } />
+                    <Route path="ShoppingPage" element={ <ShoppingPage></ShoppingPage>} />
                     
-                    <Route path="/*" element={ <Navigate to="/home" replace /> } />
+                    <Route path="/*" element={ <Navigate to="/ShoppingPage" replace /> } />
                 </Routes>
 
             </div>
